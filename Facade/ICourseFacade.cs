2c4e57ea0061eca6526db16a84e36ec6a2075bba
dungntbh <dungntbh01233@fpt.edu.vn>
2025-03-29 23:18:26 +0700

    code đây

--- Facade/ICourseFacade.cs
+++ Facade/ICourseFacade.cs
@@ -4,6 +4,10 @@ namespace ManagerSIMS.Facade
 {
     public interface ICourseFacade
     {
-       
+        IEnumerable<Course> GetAllCourses();
+        Course GetCourseById(int courseId);
+        void CreateCourse(Course course);
+        void EditCourse(Course course);
+        void RemoveCourse(int courseId);
     }
 }
