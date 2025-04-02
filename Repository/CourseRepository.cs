a1e0994d9815af867afeef450f9a0e4d1702ade8
huydv01237 <huydvbh01237@fpt.edu.vn>
2025-04-02 22:22:27 +0700

    main

--- Repository/CourseRepository.cs
+++ Repository/CourseRepository.cs
@@ -4,9 +4,48 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ManagerSIMS.Repository
 {
-    public class CourseRepository 
+    public class CourseRepository : ICourseRepository
     {
-        
+        private readonly ApplicationDbContext _context;
+
+        public CourseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Course> GetAllCourses()
+        {
+            return _context.Courses.ToList();
+        }
+
+        public Course GetCourseById(int courseId)
+        {
+            return _context.Courses.Find(courseId);
+        }
+
+        public void AddCourse(Course course)
+        {
+            _context.Courses.Add(course);
+        }
+
+        public void UpdateCourse(Course course)
+        {
+            _context.Entry(course).State = EntityState.Modified;
+        }
+
+        public void DeleteCourse(int courseId)
+        {
+            var course = _context.Courses.Find(courseId);
+            if (course != null)
+            {
+                _context.Courses.Remove(course);
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
     }
 
 }
